0cab27b11044b3e6590b98a92b9b83b00f3bc32e
itc-s24010 <s24010@std.it-college.ac.jp>
2025-10-15 12:36:59 +0900

    p140まで。デバック作業中

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -1,16 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
-
 import styles from "./index.module.css";
 import Category from "../Category";
 import Date from "../Date";
 import type { News } from "@/app/_libs/microcms";
-
-
 type Props = {
     news: News[];
 };
-
 export default function NewsList({ news }: Props) {
     if (news.length === 0) {
         return <p>記事がありません。</p>;
@@ -20,13 +16,23 @@ export default function NewsList({ news }: Props) {
             {news.map((article) => (
                 <li key={article.id} className={styles.list}>
                     <Link href={`/news/${article.id}`} className={styles.link}>
+                    {article.thumbnail ? (
                         <Image
-                            className={styles.image}
-                            src="/no-image.png"
-                            alt="No Image"
-                            width={1200}
-                            height={630}
+                        src={article.thumbnail.url}
+                        alt=""
+                        className={styles.image}
+                        width={article.thumbnail.width}
+                        height={article.thumbnail.height}
                         />
+                    ) : (
+                        <Image
+                        className={styles.image}
+                        src="/no-image.png"
+                        alt="No Image"
+                        width={1200}
+                        height={630}
+                    />
+                    )}
                         <dl className={styles.content}>
                             <dt className={styles.title}>{article.title}</dt>
                             <dd className={styles.meta}>
@@ -35,7 +41,6 @@ export default function NewsList({ news }: Props) {
                             </dd>
                         </dl>
                     </Link>
-                    
                 </li>
             ))}
         </ul>
