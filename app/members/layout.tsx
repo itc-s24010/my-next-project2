9f8cd5c09db7d0b7b25b324d44eeee6f70a90e7f
itc-s24010 <s24010@std.it-college.ac.jp>
2025-11-11 09:34:36 +0900

    c10まで

--- app/members/layout.tsx
+++ app/members/layout.tsx
@@ -1,17 +1,21 @@
-import React from "react";
-import Sheet from "@/app/_components/Sheet";
-import Hero from "../_components/Hero";
+import { GoogleAnalytics } from '@next/third-parties/google';
+import Sheet from '@/app/_components/Sheet';
+import Hero from '@/app/_components/Hero';
+
+export const metadata = {
+  title: 'メンバー',
+};
 
 type Props = {
-    children: React.ReactNode;
+  children: React.ReactNode;
 };
-
 
 export default function RootLayout({ children }: Props) {
-    return (
-        <>
-            <Hero title="Members" sub="メンバー" />
-            <Sheet>{children}</Sheet>
-        </>
-    );
-};
+  return (
+    <>
+      <Hero title="Members" sub="メンバー" />
+      <Sheet>{children}</Sheet>
+      <GoogleAnalytics gaId="G-XXX" />
+    </>
+  );
+}
